123d52c249ba5538615d5149c0b8a3259dae34a4
Topper Bowers <topper@toppingdesign.com>
2014-04-04 11:33:40 -0400

    log the data again

--- proxy.js
+++ proxy.js
@@ -67,6 +67,7 @@ var server = http.createServer(function(req, res) {
     res.end();
 
     var output = [];
+    console.log("RECEIVED: ", data);
     var parsed = JSON.parse(data);
     parsed.forEach(function(x) {
       var name = x.host + '.' + x.plugin;
