d3792e386c4621672395f4224bd97bbd8567f1ba
Topper Bowers <topper@toppingdesign.com>
2014-04-04 10:42:17 -0400

    for now, write out all data

--- proxy.js
+++ proxy.js
@@ -67,6 +67,8 @@ var server = http.createServer(function(req, res) {
     res.end();
 
     var output = [];
+
+      console.log("DATA RECEIVED", data);
     var parsed = JSON.parse(data);
     parsed.forEach(function(x) {
       var name = x.host + '.' + x.plugin;
